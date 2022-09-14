425e16a72b2425ec54b4a97b4d7cded95b64c1ec
maggie-light <mosesmargret2@gmail.com>
2022-09-14 06:29:44 -0700

    task twelve

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 
        if (n <= 15 && n >= 0)
    {
-	for (';' num = 0; num <= n num++)
+	for (num = 0; num <= n; num++)
 {
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
