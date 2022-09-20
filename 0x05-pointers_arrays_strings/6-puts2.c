5db0e06fabf432bcac2bd0a0133190af0a3df02c
maggie-light <mosesmargret2@gmail.com>
2022-09-20 15:41:53 +0100

    character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 -  function that prints every other character of a string
+ * @str: str parameter
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
