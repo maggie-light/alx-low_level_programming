872a74b2f615c2a4a005c6415d4611d9d91c4896
maggie-light <mosesmargret2@gmail.com>
2022-09-20 15:48:35 +0100

    error fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 -  function that prints every other character of a string
  * @str: str parameter
@@ -7,7 +8,7 @@ void puts2(char *str)
 {
 	int len, i;
 
-	len = str(str);
+	len = strlen(str);
 	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
