054c62dd0e9d7f305846015d2c947c5ae6bb8bf4
maggie-light <mosesmargret2@gmail.com>
2022-09-20 21:29:31 +0100

    fixed error

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,11 +9,11 @@ int main(void)
 	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(0));
+	rand(time(0));
 
 	while (sum < 2772)
 	{
-		password[index] = 33 + srand() % 94;
+		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
 	password[index] = '\0';
