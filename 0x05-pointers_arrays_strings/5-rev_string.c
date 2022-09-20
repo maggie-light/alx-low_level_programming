08dc8deae35796f79569e9a14ae7c21120affb47
maggie-light <mosesmargret2@gmail.com>
2022-09-20 15:28:32 +0100

    reversed string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,10 @@ void rev_string(char *s)
 	char tmp;
 	int i, lenght1, lenght2;
 
-	lenght = 0;
-	lenght = 0;
-	while (s[lenght] != '\0')
-		lenght++;
+	lenght1 = 0;
+	lenght2 = 0;
+	while (s[lenght1] != '\0')
+		lenght1++;
 	lenght2 = lenght1 - 1;
 	for (i = 0; i < lenght1 / 2; i++)
 	{
