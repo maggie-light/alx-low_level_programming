2c354484e6b4dccdde470ce206dd508ee08a9ceb
maggie-light <mosesmargret2@gmail.com>
2022-09-22 14:10:57 +0100

    task three

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int x;
+
+	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
 	{
-		s1++;
-		s2++;
+		if (s1[x] != s2[x])
+			return (s1[x] - s2[x]);
 	}
-	return (*s1 - *s2);
+	return (0);
 }
 
