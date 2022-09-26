405f9c132ca245184d5fcf29ef5f748e3432fdf6
maggie-light <mosesmargret2@gmail.com>
2022-09-26 12:56:06 +0100

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int m;
+	unsigned int a;
 
-	for (m = 0; m < n; m++)
-		dest[m] = src[m];
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
 	return (dest);
 }
 
