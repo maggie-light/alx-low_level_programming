96f6e6c49c445c9cb79748f543f683a1bf91e4a9
maggie-light <mosesmargret2@gmail.com>
2022-09-26 21:30:03 +0100

    string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a = 0;
-	int b;
+	int a = 0, b;
 
 	while (s[a])
 		a++;
 	for (b = 0; b < a; b++)
 	{
 		if (c == s[b])
-		s += b;
+			s += b;
 		return (s);
 	}
 	return ('\0');
