ea2c97294b0ece3b479af6009dd741b9062d7781
maggie-light <mosesmargret2@gmail.com>
2022-09-28 10:57:45 +0100

    string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: parameter
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	char s;
+		_putchar("%s. \n", s);
+	return (0);
+}
