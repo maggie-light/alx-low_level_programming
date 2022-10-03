90f0f301e9eb5aa1a4bf6f593bb7953512615344
maggie-light <mosesmargret2@gmail.com>
2022-10-03 13:39:24 +0100

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] || s2[i]; i++)
-		len++
+		len++;
 	concat_s = malloc(sizeof(char) * len);
 	if (concat_s == NULL)
 		return (NULL);
