acaf7d376044e98cda49462067b877c9aadb1a13
maggie-light <mosesmargret2@gmail.com>
2022-10-20 21:16:36 +0100

    task six

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+extern printf
+
+section .text
+global main
+
+main:
+push
+
+mov rdi,fmt
+mov rsi,msg
+mov rax,0
+call printf
+
+pop rbp
+
+mov rax,0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
+
+
