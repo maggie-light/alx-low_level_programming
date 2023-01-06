b230c3880e866e98c415c11cb646410da27ad213
maggie-light <mosesmargret2@gmail.com>
2023-01-06 22:27:17 +0100

    task zero

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Create a hash table
+ * @size: The size of the array
+ *
+ * Return: NULL or pointer
+ */
+hash_table_t *hash_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size[i] = NULL;
+	return (ht);
+}
+			
---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,6 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
+
